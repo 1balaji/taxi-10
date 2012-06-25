742c30b4f5c568293d945363353d134f3e05fe18
Dan Cirnat <dan@alt.md>
2012-06-25 22:22:42 +0300

    display map

--- src/com/taxi/MapActivity.java
+++ src/com/taxi/MapActivity.java
@@ -7,11 +7,12 @@ package com.taxi;
 import android.app.Activity;
 import android.os.Bundle;
 
+
 /**
  *
  * @author Dan
  */
-public class MapActivity extends Activity {
+public class MapActivity extends com.google.android.maps.MapActivity {
 
     /**
      * Called when the activity is first created.
@@ -20,5 +21,10 @@ public class MapActivity extends Activity {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.map);      
+    }
+
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
     }
 }
